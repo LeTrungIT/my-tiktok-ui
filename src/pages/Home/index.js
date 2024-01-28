2edf64e7270bbf33a046e93d905fd4d6af8c203e
LeTrungIT <letrantrung711@gmail.com>
2024-01-28 14:59:32 +0700

    [fix] cập nhật giao diện mới

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Trang chủ TikTok Home Page</h1>;
+    return <h1>Trang chủ TikTok - Home Page</h1>;
 }
 
 export default Home;
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 function Profile() {
-    return <h1>Profile Page</h1>;
+    return <h1>Profile Page - Đây là trang thông tin tài khoản của bạn</h1>;
 }
 
 export default Profile;
